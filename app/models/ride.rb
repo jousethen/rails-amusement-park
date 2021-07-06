69c5fe191ffd94adec06ed04d0e543892d75e9bc
jousethen <jthen93@gmail.com>
2021-07-05 20:54:19 -0400

    I probably should be committing more for these assignments. I'll just start here. Passing models tests and completed migrations

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,41 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    message = "";
+    
+
+    if enough_tickets? && tall_enough?
+      
+      tickets = self.user.tickets - self.attraction.tickets
+      nausea = self.user.nausea + self.attraction.nausea_rating
+      happiness = self.user.happiness + self.attraction.happiness_rating
+      
+      self.user.update(tickets: tickets, nausea: nausea, happiness: happiness)
+      self.user.save
+      message = "Enjoy your ride #{user.name}"
+
+    elsif !enough_tickets? && !tall_enough?
+      message = "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+    
+    elsif !enough_tickets?
+      message = "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+    
+    else
+      message = "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    end
+
+    message
+  end
+
+  private
+  
+  def enough_tickets?
+    return true unless self.attraction.tickets > self.user.tickets
+  end
+
+  def tall_enough?
+    return true unless self.attraction.min_height > self.user.height
+  end
 end
---
+++ db/migrate/20210706004414_add_default_admin_value.rb
@@ -0,0 +1,5 @@
+class AddDefaultAdminValue < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :admin, :boolean, :default => false
+  end
+end
